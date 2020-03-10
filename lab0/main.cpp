2c3d8ad98f738fbe0502a58a2ec0c19e528b54f2
YaTing <felixchuang3020@gmial.com>
2020-03-11 02:20:16 +0800

    xx

---
+++ lab0/main.cpp
@@ -0,0 +1,22 @@
+#include "mbed.h"
+
+DigitalOut redLED(LED_RED);
+DigitalOut greenLED(LED_GREEN);
+
+// int Led(DigitalOut & ledpin);
+
+int main()
+{
+    redLED = 1;
+    greenLED = 1;
+    while (true){
+        for(int i = 0; i < 6; ++i){
+            redLED = !redLED;
+            wait(0.2f);
+        }
+        for(int i = 0; i < 4; ++i){
+            greenLED = !greenLED;
+            wait(0.2f);
+        }        
+    }
+}
